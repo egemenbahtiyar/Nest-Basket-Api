4a99a04cc9c846f9b30873162d94b64a523f4246
Egemen Bahtiyar <e.bahtiyar51@gmail.com>
2022-08-22 15:26:38 +0300

    Cart ve CartItems modüle register edildi

--- src/cart/cart.module.ts
+++ src/cart/cart.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CartController } from './cart.controller';
 import { CartService } from './cart.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cart } from './entitiy/cart.entitiy';
+import { CartItem } from './entitiy/cartItem.entitiy';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Cart, CartItem])],
   controllers: [CartController],
   providers: [CartService],
 })
