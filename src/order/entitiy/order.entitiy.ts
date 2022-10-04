2d4b0a4c4f36783890352406c9ea63f9d302a2e6
Egemen Bahtiyar <e.bahtiyar51@gmail.com>
2022-10-04 16:10:57 +0300

    Order state eklendi

--- src/order/entitiy/order.entitiy.ts
+++ src/order/entitiy/order.entitiy.ts
@@ -8,6 +8,13 @@ import {
 import { OrderItem } from './orderItem.entitiy';
 import { User } from '../../user/entitiy/user.entitiy';
 
+export enum OrderState {
+  waiting = 0,
+  canceled = 1,
+  completed = 2,
+  evaluated = 3,
+}
+
 @Entity()
 export class Order {
   @PrimaryGeneratedColumn()
@@ -21,4 +28,11 @@ export class Order {
 
   @OneToMany(() => OrderItem, (orderItem) => orderItem.order)
   OrderItems: OrderItem[];
+
+  @Column({
+    type: 'enum',
+    enum: OrderState,
+    default: OrderState.waiting,
+  })
+  orderState: OrderState;
 }
